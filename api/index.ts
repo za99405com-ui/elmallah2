4237a56a882cd194904fc067f6ea307709543e17
za99405com-ui <za99405@gmail.com>
2026-09-02 16:47:01 +0300

    Fix vercel api entry

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../server';
-export default app;
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  return app(req, res);
+};
