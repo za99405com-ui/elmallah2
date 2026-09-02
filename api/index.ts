d5402726f96e7df9e3e3d6f845fb349f25c1ecae
za99405com-ui <za99405@gmail.com>
2026-09-02 16:51:02 +0300

    Fix Vercel routing and serverless function

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../server';
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
-};
+}
