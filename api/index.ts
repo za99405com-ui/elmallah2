28dd96f65d27c13a55a96b695a226a069bbb06f9
za99405com-ui <za99405@gmail.com>
2026-09-03 07:00:23 +0300

    Remove conflicting index.ts and force clean JS entry

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server.js';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return app(req, res);
-};
